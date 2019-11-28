c53ce9e5fb36ecc821e7524939faafb67e00ac89
Chaturvedi, Sumeet <sumeet_chaturvedi@optum.com>
2019-11-28 15:33:23 +0530

    deleting unwanted files
    deleting unwanted files

--- src/main/java/vehicle.py
+++
@@ -1,9 +0,0 @@
-
-class Vehicle:
-    
-    def __init__(self,rego,model,color,price):
-        self.rego = rego
-        self.model = model
-        self.color = color
-        self.price = price
-        
